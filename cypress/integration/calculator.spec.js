936325e9c56588ce6f5fbd758539405cd7e4ba83
DSoutter <duncan.soutter@gmail.com>
2021-08-31 16:54:30 +0100

    multiplying numbers works

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -8,9 +8,12 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '2')
   })
 
-  it('running total should display numbers clicked', () => {
-    cy.get('#number2').click();
-    cy.get('')
+  it('running total should display result of a sum', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain','21')
   })
 
 })
